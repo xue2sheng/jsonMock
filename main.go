f1d62711d1b84ad43d71e3d496deeadee1ec0cf6
Andres Sevillano <andres@chameleon.ad>
2017-03-15 14:16:07 +0000

    Binding 'global' map to fcgihandler

--- main.go
+++ main.go
@@ -18,13 +18,14 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-// helper for HTTP handler queries
-type customHandler struct {
-	cmux http.Handler
-}
-
 // Request Response map
 type RequestResponseMap map[string]string
+
+// helper for HTTP handler queries
+type customHandler struct {
+	cmux  http.Handler
+	rrmap *RequestResponseMap
+}
 
 // RequestJsonSchema to validate requests
 var RequestJsonSchemaFile = "requestJsonSchema.json"
@@ -39,10 +40,6 @@ var MockRequestResponseFile = "requestResponseMap.json"
 var DebugParameter = "debug"
 
 func main() {
-	mux := mux.NewRouter()
-	// bind cmux to mx(route)
-	fcgiHandler := &customHandler{cmux: mux}
-	mux.Path("/").Handler(fcgiHandler)
 
 	host, port, mockRequestResponseFile, requestJsonSchemaFile, responseJsonSchemaFile := cmdLine()
 	log.Println("Launched " + host + ":" + port + " MockRequestResponseFile=" + mockRequestResponseFile +
@@ -53,6 +50,11 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Number of faked request/response: %d", len(reqresmap))
+
+	mux := mux.NewRouter()
+	// bind cmux to mx(route)
+	fcgiHandler := &customHandler{cmux: mux, rrmap: &reqresmap}
+	mux.Path("/").Handler(fcgiHandler)
 
 	listener, _ := net.Listen("tcp", host+":"+port) // see nginx.conf
 	if err := fcgi.Serve(listener, fcgiHandler); err != nil {
@@ -105,6 +107,154 @@ func cmdLine() (string, string, string, string, string) {
 	return hostArg, portArg, mockRequestResponseFile, requestJsonSchemaFile, responseJsonSchemaFile
 }
 
+// validate fake request response map against their json schemas
+func validateMockRequestResponseFile(mockRequestResponseFile string, requestJsonSchemaFile string, responseJsonSchemaFile string) (RequestResponseMap, error) {
+	var err error
+	var reqresmap RequestResponseMap = make(map[string]string)
+
+	mock, err := validateMockInput(mockRequestResponseFile)
+	if err != nil {
+		return reqresmap, err
+	}
+
+	req, err := ioutil.ReadFile(requestJsonSchemaFile)
+	if err != nil {
+		log.Fatal(err)
+		return reqresmap, errors.New("Unable to read Request Json Schema File.")
+	}
+
+	res, err := ioutil.ReadFile(responseJsonSchemaFile)
+	if err != nil {
+		log.Fatal(err)
+		return reqresmap, errors.New("Unable to read Response Json Schema File.")
+	}
+
+	reqJsonSchema := gojsonschema.NewStringLoader(string(req))
+	resJsonSchema := gojsonschema.NewStringLoader(string(res))
+
+	type ReqRes struct {
+		Req, Res string
+	}
+	dec := json.NewDecoder(strings.NewReader(string(mock)))
+
+	err = ignoreFirstBracket(dec)
+	if err != nil {
+		return reqresmap, err
+	}
+
+	// read object {"req": string, "res": string}
+	for dec.More() {
+		var rr ReqRes
+		err = dec.Decode(&rr)
+		if err != nil {
+			log.Fatal(err)
+			return reqresmap, errors.New("Unable to process object at Mock Request Response File")
+		}
+		fmt.Printf("%v -> %v\n", rr.Req, rr.Res)
+
+		if !validateRequest(reqJsonSchema, rr.Req) {
+			continue
+		}
+
+		if !validateResponse(resJsonSchema, rr.Res) {
+			continue
+		}
+
+		// add pair to the map but after compacting those json
+		reqBuffer := new(bytes.Buffer)
+		err = json.Compact(reqBuffer, []byte(rr.Req))
+		if err != nil {
+			log.Fatal(err)
+			log.Println("This request will be ignored")
+			continue
+		}
+		resBuffer := new(bytes.Buffer)
+		err = json.Compact(resBuffer, []byte(rr.Res))
+		if err != nil {
+			log.Fatal(err)
+			log.Println("That response will be ignored")
+			continue
+		}
+		key := reqBuffer.String()
+		value := resBuffer.String()
+		reqresmap[key] = value
+
+	}
+
+	err = ignoreLastBracket(dec)
+	if err != nil {
+		return reqresmap, err
+	}
+
+	// return result
+	if len(reqresmap) == 0 {
+		err = errors.New("Unable to validate any entry at Mock Request Response File")
+	}
+	return reqresmap, err
+}
+
+// validation request
+func validateRequest(reqJsonSchema gojsonschema.JSONLoader, rrReq string) bool {
+
+	result, err := gojsonschema.Validate(reqJsonSchema, gojsonschema.NewStringLoader(rrReq))
+	if err != nil {
+		log.Fatal(err)
+		log.Println("This request will be ignored")
+		return false
+	}
+	if !result.Valid() {
+		log.Println("Request is not valid. See errors: ")
+		for _, desc := range result.Errors() {
+			log.Printf("- %s\n", desc)
+		}
+		log.Println("That request will be ignored")
+		return false
+	}
+
+	return true
+}
+
+// validation response
+func validateResponse(resJsonSchema gojsonschema.JSONLoader, rrRes string) bool {
+
+	result, err := gojsonschema.Validate(resJsonSchema, gojsonschema.NewStringLoader(rrRes))
+	if err != nil {
+		log.Fatal(err)
+		log.Println("This response will be ignored")
+		return false
+	}
+	if !result.Valid() {
+		log.Println("Response is not valid. See errors: ")
+		for _, desc := range result.Errors() {
+			log.Printf("- %s\n", desc)
+		}
+		log.Println("That response will be ignored")
+		return false
+	}
+
+	return true
+}
+
+// ignore first bracket when json mock Request Response file is decoded
+func ignoreFirstBracket(dec *json.Decoder) error {
+	_, err := dec.Token()
+	if err != nil {
+		log.Fatal(err)
+		return errors.New("Unable to process first token at Mock Request Response File")
+	}
+	return nil
+}
+
+// ignore last bracket when json mock Request Response file is decoded
+func ignoreLastBracket(dec *json.Decoder) error {
+	_, err := dec.Token()
+	if err != nil {
+		log.Fatal(err)
+		return errors.New("Unable to process last token at Mock Request Response File")
+	}
+	return nil
+}
+
 // validate just mock input
 func validateMockInput(mockRequestResponseFile string) ([]byte, error) {
 
@@ -152,117 +302,6 @@ func validateMockInput(mockRequestResponseFile string) ([]byte, error) {
 
 	// success
 	return mock, nil
-}
-
-// validate fake request response map against their json schemas
-func validateMockRequestResponseFile(mockRequestResponseFile string, requestJsonSchemaFile string, responseJsonSchemaFile string) (RequestResponseMap, error) {
-	var err error
-	var reqresmap RequestResponseMap = make(map[string]string)
-
-	mock, err := validateMockInput(mockRequestResponseFile)
-	if err != nil {
-		return reqresmap, err
-	}
-
-	req, err := ioutil.ReadFile(requestJsonSchemaFile)
-	if err != nil {
-		log.Fatal(err)
-		return reqresmap, errors.New("Unable to read Request Json Schema File.")
-	}
-
-	res, err := ioutil.ReadFile(responseJsonSchemaFile)
-	if err != nil {
-		log.Fatal(err)
-		return reqresmap, errors.New("Unable to read Response Json Schema File.")
-	}
-
-	reqJsonSchema := gojsonschema.NewStringLoader(string(req))
-	resJsonSchema := gojsonschema.NewStringLoader(string(res))
-
-	type ReqRes struct {
-		Req, Res string
-	}
-	dec := json.NewDecoder(strings.NewReader(string(mock)))
-	// read open bracket
-	_, err = dec.Token()
-	if err != nil {
-		log.Fatal(err)
-		return reqresmap, errors.New("Unable to process first token at Mock Request Response File")
-	}
-	// read object {"req": string, "res": string}
-	for dec.More() {
-		var rr ReqRes
-		err = dec.Decode(&rr)
-		if err != nil {
-			log.Fatal(err)
-			return reqresmap, errors.New("Unable to process object at Mock Request Response File")
-		}
-		fmt.Printf("%v -> %v\n", rr.Req, rr.Res)
-
-		// validation request
-		result, err := gojsonschema.Validate(reqJsonSchema, gojsonschema.NewStringLoader(string(rr.Req)))
-		if err != nil {
-			log.Fatal(err)
-			log.Println("This request will be ignored")
-			continue
-		}
-		if !result.Valid() {
-			log.Println("Request is not valid. See errors: ")
-			for _, desc := range result.Errors() {
-				log.Printf("- %s\n", desc)
-			}
-			log.Println("That request will be ignored")
-			continue
-		}
-
-		// validation response
-		result, err = gojsonschema.Validate(resJsonSchema, gojsonschema.NewStringLoader(string(rr.Res)))
-		if err != nil {
-			log.Fatal(err)
-			log.Println("This response will be ignored")
-			continue
-		}
-		if !result.Valid() {
-			log.Println("Response is not valid. See errors: ")
-			for _, desc := range result.Errors() {
-				log.Printf("- %s\n", desc)
-			}
-			log.Println("That response will be ignored")
-			continue
-		}
-
-		// add pair to the map but after compacting those json
-		reqBuffer := new(bytes.Buffer)
-		err = json.Compact(reqBuffer, []byte(rr.Req))
-		if err != nil {
-			log.Fatal(err)
-			log.Println("This request will be ignored")
-			continue
-		}
-		resBuffer := new(bytes.Buffer)
-		err = json.Compact(resBuffer, []byte(rr.Res))
-		if err != nil {
-			log.Fatal(err)
-			log.Println("That response will be ignored")
-			continue
-		}
-		key := reqBuffer.String()
-		value := resBuffer.String()
-		reqresmap[key] = value
-
-	}
-	// read close bracket
-	_, err = dec.Token()
-	if err != nil {
-		log.Fatal(err)
-		return reqresmap, errors.New("Unable to process last token at Mock Request Response File")
-	}
-
-	// return result
-	if len(reqresmap) == 0 {
-		err = errors.New("Unable to validate any entry at Mock Request Response File")
-	}
-	return reqresmap, err
 }
 
 // must have at least ServeHTTP(), otherwise you will get this error
